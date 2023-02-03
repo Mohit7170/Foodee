596c72a41dd075a407c0bb561483e8a42f89e9f6
Sunil <sunil@webhopers.com>
2023-02-03 16:57:52 +0530

    splash updated

--- app/src/main/java/com/example/myapplication/modals/local/MenuItems.kt
+++ app/src/main/java/com/example/myapplication/modals/local/MenuItems.kt
@@ -1,24 +1,54 @@
 package com.example.myapplication.modals
 
 import android.content.Context
+import android.os.Handler
+import android.os.Looper
+import androidx.core.os.postDelayed
 import com.example.myapplication.R
 import com.example.myapplication.classes.AppDatabase
 import com.example.myapplication.modals.db.MenuItem
+
+
 class DemoMenuItems {
 
     companion object {
         fun getItems(context: Context): List<MenuItem> {
             val items = AppDatabase.getInstance(context).menuDao().getAll()
+
             if (items.isEmpty()) addIteToDb(context)
             return items
         }
 
-        private fun addIteToDb(context: Context) {
+        fun addIteToDb(context: Context) {
             AppDatabase.getInstance(context).menuDao().insertAll(
-                MenuItem(itemId = 0, itemName = "Burger", itemPrice = 100.0, itemImage = R.drawable.img_1, isInCart = false),
-                MenuItem(itemId = 1, itemName = "Pizza", itemPrice = 80.0, itemImage = R.drawable.img_2, isInCart = false),
-                MenuItem(itemId = 2, itemName = "Rice", itemPrice = 150.0, itemImage = R.drawable.img_3, isInCart = false),
-                MenuItem(itemId = 3, itemName = "Food", itemPrice = 180.0, itemImage = R.drawable.img_4, isInCart = false),
+                MenuItem(
+                    itemId = 0,
+                    itemName = "Burger",
+                    itemPrice = 100.0,
+                    itemImage = R.drawable.img_1,
+                    isInCart = false
+                ),
+                MenuItem(
+                    itemId = 1,
+                    itemName = "Pizza",
+                    itemPrice = 80.0,
+                    itemImage = R.drawable.img_2,
+                    isInCart = false
+                ),
+                MenuItem(
+                    itemId = 2,
+                    itemName = "Rice",
+                    itemPrice = 150.0,
+                    itemImage = R.drawable.img_3,
+                    isInCart = false
+                ),
+                MenuItem(
+                    itemId = 3,
+                    itemName = "Food",
+                    itemPrice = 180.0,
+                    itemImage = R.drawable.img_4,
+                    isInCart = false
+                ),
             )
 
         }
---
+++ app/src/main/java/com/example/myapplication/ui/activities/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication.ui.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.core.os.postDelayed
+import com.example.myapplication.R
+import com.example.myapplication.modals.DemoMenuItems
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        DemoMenuItems.addIteToDb(this)
+
+        Handler(Looper.getMainLooper()).postDelayed(2000) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
+}
